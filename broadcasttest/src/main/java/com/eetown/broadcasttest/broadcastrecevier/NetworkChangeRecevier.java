535ce0780370db7ca906e69f961a4d567d75b669
zengjunxiang <zengjunxiang@eetown.com>
2019-03-28 10:25:10 +0800

    NetworkChangeRecevier

---
+++ broadcasttest/src/main/java/com/eetown/broadcasttest/broadcastrecevier/NetworkChangeRecevier.java
@@ -0,0 +1,12 @@
+package com.eetown.broadcasttest.broadcastrecevier;
+
+/**
+ * Created with IDEA
+ *
+ * @Author:Elvis
+ * @Date:2019/3/28
+ * @Time:10:11
+ * @Description:
+ */
+public class NetworkChangeRecevier {
+}
