0a314e0583ae8f8ad09417b25464d2f5d4358cfe
zengjunxiang <zengjunxiang@eetown.com>
2019-03-28 11:37:02 +0800

    为开机完成广播 设置优先级

--- broadcasttest/src/main/java/com/eetown/broadcasttest/broadcastrecevier/BootCompleteRecevier.java
+++ broadcasttest/src/main/java/com/eetown/broadcasttest/broadcastrecevier/BootCompleteRecevier.java
@@ -5,7 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.widget.Toast;
 
-public class BootCompleteReceiver extends BroadcastReceiver {
+public class BootCompleteRecevier extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
