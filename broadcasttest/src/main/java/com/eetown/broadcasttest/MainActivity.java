a8c3add3c93b6e59fbbae78b85dc1154f41d7ef3
zengjunxiang <zengjunxiang@eetown.com>
2019-03-28 10:53:35 +0800

    optimization network change hint

--- broadcasttest/src/main/java/com/eetown/broadcasttest/MainActivity.java
+++ broadcasttest/src/main/java/com/eetown/broadcasttest/MainActivity.java
@@ -1,13 +1,38 @@
 package com.eetown.broadcasttest;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.eetown.broadcasttest.broadcastrecevier.NetworkChangeRecevier;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final String TAG = "MainActivity";
+    private final String NET_CONNECT_CHANGGE = "android.net.conn.CONNECTIVITY_CHANGE";
+    private IntentFilter mIntentFilter;
+    private NetworkChangeRecevier mNetworkChangeRecevier;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mIntentFilter = new IntentFilter();
+        mIntentFilter.addAction(NET_CONNECT_CHANGGE);
+
+        mNetworkChangeRecevier = new NetworkChangeRecevier();
+
+        //动态注册的广播,一定要取消
+        registerReceiver(mNetworkChangeRecevier, mIntentFilter);
     }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(mNetworkChangeRecevier);
+    }
+
 }
--- broadcasttest/src/main/java/com/eetown/broadcasttest/broadcastrecevier/NetworkChangeRecevier.java
+++ broadcasttest/src/main/java/com/eetown/broadcasttest/broadcastrecevier/NetworkChangeRecevier.java
@@ -1,5 +1,15 @@
 package com.eetown.broadcasttest.broadcastrecevier;
 
+import android.annotation.SuppressLint;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.widget.Toast;
+
+import com.eetown.broadcasttest.MainActivity;
+
 /**
  * Created with IDEA
  *
@@ -8,5 +18,23 @@ package com.eetown.broadcasttest.broadcastrecevier;
  * @Time:10:11
  * @Description:
  */
-public class NetworkChangeRecevier {
+public class NetworkChangeRecevier extends BroadcastReceiver {
+
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO: 2019/3/28  somethings
+        //  Toast.makeText(context, "NetWork had changed", Toast.LENGTH_LONG).show();
+
+        //better
+        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+
+        if (networkInfo != null && networkInfo.isConnected()) {
+            Toast.makeText(context, "NetWork is  connected", Toast.LENGTH_LONG).show();
+        }else {
+            Toast.makeText(context, "NetWork is  unavailable", Toast.LENGTH_LONG).show();
+        }
+
+    }
 }
